3abda26031183581c25b06826fbd97442b683532
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-03 04:10:52 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -64,6 +64,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 end
 
+# Question 1 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(hash)
@@ -79,3 +80,8 @@ def key_for_min_value(hash)
 end
 
 key_for_min_value(hash)
+
+# Question 2 
+
+
+
